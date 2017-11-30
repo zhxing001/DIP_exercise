56afb4b171e1b76fc880636bdf11c4707dd460a5
zhxing <zhxing_cas@163.com>
2017-11-30 09:20:56 +0800

    some changes

--- FOI/FOI_Hist.m
+++ FOI/FOI_Hist.m
@@ -3,7 +3,7 @@
 
 clear all;
 thresh=0.4;          %判断是否为关键帧的阈值，这个根据不同的视频场景要有微调
-path='C:\Users\zhxing\Desktop\cv_tracking\附件3-检测前景视频\curtain\Curtain.avi';
+path='C:\Users\zhxing\Desktop\cv_tracking\附件3-检测前景视频\overpass\overpass.avi';
 video = VideoReader(path);                  %读取视频
 frame_n=video.NumberOfFrames;               %视频帧数
 video_bg=[];                 %建立一个图像用来存储背景
@@ -27,6 +27,7 @@ siz=size(video_frame_g);             %大小
 video_bg=median(s);                  % 求幅图像的中间值
 video_bg=reshape(video_bg,siz);       %重构图像
 video_bg=uint8(video_bg);             %转化成8阶灰度
+figure,title('Curtain');
 subplot(2,2,1);
 imshow(video_bg);                    %显示中值图像，也就是建模得到的背景
 title('背景');
@@ -36,7 +37,7 @@ bar(hist_bg);
 title('背景直方图');
 
 hist_cha=zeros(1,frame_n);      %来存直方图的差值.每一帧都存下来
-
+tic;
 for i=1:frame_n                              %直接用每一帧和背景做差。
     current=read(video,i);                 
     current=rgb2gray(current);
@@ -44,14 +45,18 @@ for i=1:frame_n                              %直接用每一帧和背景做差。
     hist_cha(i)=sum(abs(hist_bg_frame-hist_bg));       %求直方图差距，这里用的最简单的对应bin的差的绝对值之和
     subplot(2,2,3);
     imshow(current);
-    title('当前视频');
+    title('当前帧');
     subplot(2,2,4);
     plot(hist_cha);
-    title('当前帧与背景直方图差距');             %这里是绝对值。不是相对值
+    title('当前帧与背景直方图相似度');             %这里是绝对值。不是相对值
 end
 
+all_time=toc;
+disp('处理的帧率为：');
+floor(frame_n/floor(toc)),disp('fps')
+
 hist_cha=hist_cha/max(hist_cha);      %归一化，可以简化阈值选择
-plot(hist_cha);                       %归一化以后的直方图之差
+%plot(hist_cha);                       %归一化以后的直方图之差
 index=find(hist_cha>thresh);          %找到符合阈值条件的帧的标号
 
 index_size=size(index);               %FOI的帧数
@@ -62,6 +67,6 @@ open(object1);
 for i=1:1:index_size(2)
     frame_index=read(video,index(i));
     writeVideo(object1, frame_index);
-    imshow(frame_index); 
+    %imshow(frame_index); 
 end
 close(object1);
