3f124c17b15329838810e0872f582466e2c3a66d
zhxing <zhxing_cas@163.com>
2018-08-19 23:27:31 +0800

    en

---
+++ opencv_c++/haze_move/FaseDeFog/arctan.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<math.h>
+
+double arctan(const double &d,const double &f)
+{
+	return atan(d / (2 * f));
+}
+
--- opencv_c++/haze_move/FaseDeFog/main.cpp
+++ opencv_c++/haze_move/FaseDeFog/main.cpp
@@ -1,11 +1,12 @@
 #include"FastDehaze.h"
+#include"arctan.h"
 
 
 
 
 int main()
 {
-	Mat img = imread("12.jpg");
+	/*Mat img = imread("12.jpg");
 	imshow("source_img", img);
 	double start, end;
 	int count = 10;
@@ -14,9 +15,10 @@ int main()
 		start = static_cast<double>(getTickCount());
 		auto mat = FastDehaze(img, 1.3, 15);
 		cout << ((double)getTickCount() - start) / getTickFrequency() << endl;
-	}
+	}*/
 	//imshow("defog", mat);
-	waitKey();
+	//waitKey();
+	cout << arctan(1.2, 3.0) << endl;
 
 	return 0;
 }
